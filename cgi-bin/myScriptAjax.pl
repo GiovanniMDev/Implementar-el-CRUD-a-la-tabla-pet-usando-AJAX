5ec2cb71568bb29c4355d6376b468f6c9dd393e3
GiovanniMDev <gmejiar@unsa.edu.pe>
2024-12-08 20:31:58 -0500

    Añadiendo el archivo nuevo

--- cgi-bin/myScriptAjax.pl
+++ cgi-bin/myScriptAjax.pl
@@ -1,63 +1,58 @@
-#!/usr/bin/perl
-
+#!/usr/bin/perl -w
 use strict;
 use warnings;
-use CGI;
 use DBI;
-use JSON;
+use CGI;
+use JSON; # Si no está instalado, ejecuta "cpan JSON"
+use utf8;
 
-# Configuración de la base de datos
-my $db_host = "localhost";
-my $db_user = "root";
-my $db_pass = "password";
-my $db_name = "mascotas";
-
-# Conexión a la base de datos
-my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_pass, { RaiseError => 1, AutoCommit => 1 });
-
-# Leer parámetros CGI
 my $cgi = CGI->new;
-my $nombre = $cgi->param('name') || '';
-my $dueno  = $cgi->param('owner') || '';
-my $especie = $cgi->param('species') || '';
-my $genero = $cgi->param('sex') || '';
-my $fechaN = $cgi->param('birth') || '';
-my $fechaM = $cgi->param('death') || undef;
 
-# Preparar codificador JSON
-my $op = JSON->new->utf8->pretty(1);
+print $cgi->header('application/json;charset=UTF-8');
 
-if ($nombre && $dueno && $especie && $genero && $fechaN) {
-    eval {
-        my $sth = $dbh->prepare("INSERT INTO pet (name, owner, species, sex, birth, death) VALUES (?, ?, ?, ?, ?, ?)");
-        $sth->execute($nombre, $dueno, $especie, $genero, $fechaN, $fechaM);
-    };
-
-    if ($@) {
-        print "Content-type: application/json\n\n";
-        print $op->encode({
-            status => "error",
-            message => "Error al insertar en la base de datos: $@"
-        });
-    } else {
-        print "Content-type: application/json\n\n";
-        print $op->encode({
-            status => "success",
-            message => "Registro insertado correctamente",
-            nombrePERL => $nombre,
-            duenoPERL => $dueno,
-            especiePERL => $especie,
-            generoPERL => $genero,
-            fechaNPERL => $fechaN,
-            fechaMPERL => $fechaM,
-        });
-    }
-} else {
-    print "Content-type: application/json\n\n";
-    print $op->encode({
-        status => "error",
-        message => "Faltan parámetros obligatorios"
-    });
+my $nombre = $cgi->param('nombre'); 
+my $dueno = $cgi->param('dueno');
+my $especie = $cgi->param('especie');   
+my $genero = $cgi->param('genero');
+my $fechaN = $cgi->param('fechaN');
+my $fechaM = $cgi->param('fechaM'); 
+if ($fechaM eq ""){
+    $fechaM = undef;
 }
 
+my $op = JSON->new->utf8->pretty(1);
+my $json = $op->encode({
+    nombrePERL => $nombre,
+    duenoPERL => $dueno,
+    especiePERL => $especie,
+    generoPERL => $genero,
+    fechaNPERL => $fechaN,
+    fechaMPERL => $fechaM,
+});
+print $json;
+
+my $dsn = "DBI:mysql:database=menagerie;host=localhost";
+my $usuario = "root";
+my $password = "123";
+
+# Conexión a la base de datos
+my $dbh = DBI->connect($dsn, $usuario, $password, { RaiseError => 1, AutoCommit => 1 })
+  or die "No se pudo conectar a la base de datos: $DBI::errstr";
+
+# Asegurar que la conexión está en UTF-8
+$dbh->do("SET NAMES 'utf8'");
+
+# Crear la consulta SQL para insertar los datos
+my $sql = "INSERT INTO pet (name, owner, species, sex, birth, death)
+           VALUES (?, ?, ?, ?, ?, ?)";
+
+# Preparar la consulta SQL
+my $sth = $dbh->prepare($sql) or die "No se pudo preparar la consulta: $dbh->errstr";
+
+# Ejecutar la consulta con los datos
+$sth->execute($nombre, $dueno, $especie, $genero, $fechaN, $fechaM)
+  or die "Error al insertar los datos: $dbh->errstr";
+
+# Cerrar el statement handle y la conexión
+$sth->finish();
 $dbh->disconnect();
